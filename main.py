3b302e769dc39221a96778d64e5f652d2809e7b4
CoreProduction3 <production3.8k37+liam@coreelectronics.com.au>
2023-09-12 14:32:12 +1000

    Added filemanager for taking lots of photos

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ CS - GP17
 '''
 
 from utime import sleep_ms
+import utime
+
+import uos
+import ujson
 
 ############### HIGH LEVEL FUNCTIONS #################
 
@@ -39,9 +43,55 @@ class camera:
     
     # Set mode
     CAM_REG_COLOR_EFFECT_CONTROL = 0x27
+    
     SPECIAL_NORMAL = 0x00
     SPECIAL_BW = 0x04
     SPECIAL_GREENISH = 0x20
+    
+
+    
+    # Set Brightness
+
+    # Set Contrast
+    CAM_REG_CONTRAST_CONTROL = 0X23
+    
+    CONTRAST_MINUS_3 = 6
+    CONTRAST_MINUS_2 = 4
+    CONTRAST_MINUS_1 = 2
+    CONTRAST_DEFAULT = 0
+    CONTRAST_PLUS_1 = 1
+    CONTRAST_PLUS_2 = 3
+    CONTRAST_PLUS_3 = 5
+    
+    
+    # Set Saturation
+    
+    # Set Exposure Value
+    
+    # Set Whitebalance
+    CAM_REG_WB_MODE_CONTROL = 0X26
+    
+    WB_MODE_AUTO = 0
+    WB_MODE_SUNNY = 1
+    WB_MODE_OFFICE = 2
+    WB_MODE_CLOUDY = 3
+    WB_MODE_HOME = 4
+    
+    
+    
+        
+        
+        
+    
+    # Set Colour Effect
+    
+    # Set Sharpness
+    
+    # Set Autofocus
+    
+    # Set Image quality
+    
+    # 
     
     
     # Device addressing
@@ -59,7 +109,7 @@ class camera:
     CAM_IMAGE_PIX_FMT_RGB565 = 0x02
     CAM_IMAGE_PIX_FMT_YUV = 0x03
     
-    
+    # Resolution settings
     CAM_REG_CAPTURE_RESOLUTION = 0x21
     
     RESOLUTION_160X120 = 0X00
@@ -76,7 +126,7 @@ class camera:
     RESOLUTION_128X128 = 0X0b
     RESOLUTION_320X320 = 0X0c
 
-    # 
+    # FIFO and State setting registers
     ARDUCHIP_FIFO = 0x04
     FIFO_CLEAR_ID_MASK = 0x01
     FIFO_START_MASK = 0x02
@@ -89,12 +139,18 @@ class camera:
     FIFO_SIZE3 = 0x47
     
     SINGLE_FIFO_READ = 0x3D
+    BURST_FIFO_READ = 0X3C
+    
+    WHITE_BALANCE_WAIT_TIME_MS = 500
+    
+    FILE_MANAGER_LOG_NAME = 'filemanager.log'
     
     ##################### Callable FUNCTIONS #####################
         
-    def __init__(self, spi_bus, cs):
+    def __init__(self, spi_bus, cs, filemanager=True):
         self.cs = cs
         self.spi_bus = spi_bus
+        self.filemanager_enabled = filemanager
 
         self._write_reg(self.CAM_REG_SENSOR_RESET, self.CAM_SENSOR_RESET_ENABLE) # Reset camera
         self._wait_idle()
@@ -110,33 +166,42 @@ class camera:
         self.received_length = 0
         self.total_length = 0
         self.burst_first_flag = False
+        
+        # Tracks the AWB warmup time
+        self.start_time = utime.ticks_ms()
+        
         print('camera init')
     '''
-    Issue warning if the filepath doesnt end in .jpg (Blank) and append	
+    Issue warning if the filepath doesnt end in .jpg (Blank) and append
     Issue error if the filetype is NOT .jpg
     '''
     def capture_jpg(self):
         
-        # TODO: CLASSES CALL THE FUNCTION TO UPDATE NEW PIXEL FORMAT AND MODE
-        new_pixel_format = 0x00
-        new_resolution = self.RESOLUTION_1920X1080
+        if utime.ticks_diff(utime.ticks_ms(), self.start_time) >= self.WHITE_BALANCE_WAIT_TIME_MS:
         
-        print('Starting capture JPG')
-        # JPG, bmp ect
-        # TODO: PROPERTIES TO CONFIGURE THE PIXEL FORMAT
-        if new_pixel_format != self.pixel_format:
-            self._write_reg(self.CAM_REG_FORMAT, self.pixel_format) # Set to capture a jpg
-            self._wait_idle()
         
-            # TODO: PROPERTIES TO CONFIGURE THE RESOLUTION
-        if new_resolution != self.mode:
-            self._write_reg(self.CAM_REG_CAPTURE_RESOLUTION, new_resolution)
-            print('setting res', new_resolution)
-            self._wait_idle()
-        
-        # Start capturing the photo
-        self._set_capture()
-        print('capture jpg complete')
+            # TODO: CLASSES CALL THE FUNCTION TO UPDATE NEW PIXEL FORMAT AND MODE
+            new_pixel_format = 0x00
+            new_resolution = self.RESOLUTION_320X240
+            
+            print('Starting capture JPG')
+            # JPG, bmp ect
+            # TODO: PROPERTIES TO CONFIGURE THE PIXEL FORMAT
+            if new_pixel_format != self.pixel_format:
+                self._write_reg(self.CAM_REG_FORMAT, self.pixel_format) # Set to capture a jpg
+                self._wait_idle()
+            
+                # TODO: PROPERTIES TO CONFIGURE THE RESOLUTION
+            if new_resolution != self.mode:
+                self._write_reg(self.CAM_REG_CAPTURE_RESOLUTION, new_resolution)
+                print('setting res', new_resolution)
+                self._wait_idle()
+            
+            # Start capturing the photo
+            self._set_capture()
+            print('capture jpg complete')
+        else:
+            print('Please add a ', self.WHITE_BALANCE_WAIT_TIME_MS, ' delay to allow for white balance to run')
     
     # TODO: After reading the camera data clear the FIFO and reset the camera (so that the first time read can be used)
     def saveJPG(self,filename):
@@ -149,6 +214,8 @@ class camera:
         
         image_data_int = 0x00
         image_data_next_int = 0x00
+        if self.filemanager_enabled:
+            filename = self.filemanager(filename)
         
         while(self.received_length):
             
@@ -174,6 +241,40 @@ class camera:
                 jpg_to_write.write(image_data_next)
                 jpg_to_write.close()
     
+    '''
+    Start this alongside the camera module to save photos in a folder with a filename i.e. image-<counter>.jpg
+    * appends '_' after a word, the next number and the file format
+    '''
+    def filemanager(self,filename):
+        
+        '''
+        image_<count>.jpg=3
+        bird_<count>=3
+        =
+        {'image': 3,
+         'bird': 3}
+        '''
+        count = 0
+        files = {}
+        # Ensure file is present
+        if self.FILE_MANAGER_LOG_NAME not in uos.listdir():
+            with open(self.FILE_MANAGER_LOG_NAME, 'w') as f:
+                f.write(ujson.dumps({}))
+            
+        # Check if the filename already exists in the storage
+        with open(self.FILE_MANAGER_LOG_NAME, 'r') as f:
+            files = ujson.loads(f.read())
+            if filename in files:
+                count = files[filename] + 1
+            files[filename] = count
+            
+        # Save the updated list back to the storage
+        with open(self.FILE_MANAGER_LOG_NAME, 'w') as f:
+            f.write(ujson.dumps(files))
+            
+        new_filename = f"{filename}_{count}.jpg" if count > 0 else f"{filename}.jpg"
+        return new_filename
+
     def get_sensor_config(self):
         camera_id = self._read_reg(self.CAM_REG_SENSOR_ID);
         self._wait_idle()
@@ -256,7 +357,24 @@ class camera:
         self._write_reg(self.CAM_REG_COLOR_EFFECT_CONTROL, effect)
         self._wait_idle()
     
+    def set_white_balance(self, environment):
+        register_value = self.WB_MODE_AUTO
 
+        if environment == 'sunny':
+            register_value = self.WB_MODE_SUNNY
+        elif environment == 'office':
+            register_value = self.WB_MODE_OFFICE
+        elif environment == 'cloudy':
+            register_value = self.WB_MODE_CLOUDY
+        elif environment == 'home':
+            register_value = self.WB_MODE_HOME
+        elif self.camera_idx == '3MP':
+            print('TODO UPDATE: For best results set a White Balance setting')
+        
+        self.white_balance_mode = register_value
+        self._write_reg(self.CAM_REG_WB_MODE_CONTROL, register_value)
+        self._wait_idle()
+        
 
 
 
@@ -268,20 +386,27 @@ cs = Pin(17, Pin.OUT)
 onboard_LED = Pin(25, Pin.OUT)
 
 cam = camera(spi, cs)
-sleep_ms(2000)
-photo_counter = 0
 
-print('starting loop')
+sleep_ms(1000)
 
-# while True:
-#     if button.value() == False:
-onboard_LED.on()
-cam.capture_jpg()
-print('took photo')
-sleep_ms(2000)
-cam.saveJPG('/arducam-photo_low_res.jpg')
+# print('starting loop')
+# onboard_LED.on()
+# #cam.set_white_balance('office')
+# cam.capture_jpg()
+# print('took photo')
+# sleep_ms(1000)
+# cam.saveJPG('image')
+
+for i in range(2):
+    onboard_LED.on()
+    cam.capture_jpg()
+    sleep_ms(200)
+    cam.saveJPG('image')
+    onboard_LED.off()
+    sleep_ms(2000)
+
 # photo_counter += 1
-onboard_LED.off()
+#nboard_LED.off()
 
 
 #################################################################################################################################################
