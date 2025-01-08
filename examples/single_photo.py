d2e4856f87fb5068122feef1ba7ed4b1d7f882a3
wil-liammacleod <liammacleod@gmail.com>
2025-01-08 14:07:31 -0500

    Changed piout to ESP32S3, added documentation

--- examples/single_photo.py
+++ examples/single_photo.py
@@ -1,11 +1,33 @@
-from machine import Pin, SPI
-from Camera import *
 
-spi = SPI(0,sck=Pin(18), miso=Pin(16), mosi=Pin(19), baudrate=8000000)
+from machine import Pin, SPI
+from camera import *
+
+'''
+#################### PINOUT ####################
+Camera pin - Pico Pin
+VCC - 3V3 - red
+GND - GND - black
+SPI - 0
+SCK - GP18 - white
+MISO - RX - GP16 - brown
+MOSI - TX - GP19 - yellow
+CS - GP17 - orange
+
+Camera pin - ESP32 S3
+VCC - 3V3 - red
+GND - GND - black
+SPI - 2
+SCK - GP12 - white
+MISO - RX - GP13 - brown
+MOSI - TX - GP11 - yellow
+CS - GP17 - orange
+'''
+
+spi = SPI(2,sck=Pin(12), miso=Pin(13), mosi=Pin(11), baudrate=8000000)
 cs = Pin(17, Pin.OUT)
 
 # button = Pin(15, Pin.IN,Pin.PULL_UP)
-onboard_LED = Pin(25, Pin.OUT)
+onboard_LED = Pin(48, Pin.OUT)
 
 cam = Camera(spi, cs)
 
