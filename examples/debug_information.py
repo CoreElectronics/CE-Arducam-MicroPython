06126b7e67bf654557b6073afd2e5b0f709cfd52
wil-liammacleod <liammacleod@gmail.com>
2025-01-08 14:06:01 -0500

    Updated to use new camera lib

--- examples/debug_information.py
+++ examples/debug_information.py
@@ -1,19 +1,45 @@
-spi = SPI(0,sck=Pin(18), miso=Pin(16), mosi=Pin(19), baudrate=8000000)
+from machine import Pin, SPI
+from camera import *
+#Baud 8000000
+
+'''
+#################### PINOUT ####################
+Camera pin - Pico Pin
+VCC - 3V3 - red
+GND - GND - black
+SPI - 0
+SCK - GP18 - white
+MISO - RX - GP16 - brown
+MOSI - TX - GP19 - yellow
+CS - GP17 - orange
+
+Camera pin - ESP32 S3
+VCC - 3V3 - red
+GND - GND - black
+SPI - 2
+SCK - GP12 - white
+MISO - RX - GP13 - brown
+MOSI - TX - GP11 - yellow
+CS - GP17 - orange
+'''
+
+spi = SPI(2,sck=Pin(12), miso=Pin(13), mosi=Pin(11), baudrate=100000)
 cs = Pin(17, Pin.OUT)
 
 # button = Pin(15, Pin.IN,Pin.PULL_UP)
-onboard_LED = Pin(25, Pin.OUT)
+onboard_LED = Pin(48, Pin.OUT)
 
 cam = Camera(spi, cs, debug_information=True)
 '''
 RESOLUTION_160X120 = 0X00
-    RESOLUTION_320X240 = 0X01
-    RESOLUTION_640X480 = 0X02
-    RESOLUTION_1280X720 = 0X03
-    '''
+RESOLUTION_320X240 = 0X01
+RESOLUTION_640X480 = 0X02
+RESOLUTION_1280X720 = 0X03
+'''
 
 
-cam.resolution = '640X480'
+cam.resolution = '320X240'
+cam.resolution
 
 start_time_capture = utime.ticks_ms()
 
